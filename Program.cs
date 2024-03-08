9896fafc089299fadb48eb69485e210800299f51
kingabreel <gabrielyury441@gmail.com>
2024-03-08 13:53:37 -0300

    feat: desafio 03

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ public class Program
     {
         Atividade01();
         Atividade02();
+        Atividade03();
     }
 
     //Desafio 01
@@ -24,18 +25,41 @@ public class Program
         Console.WriteLine(SOMA);
     } 
 
+    //Desafio 02
     static private void Atividade02()
     {
         Console.WriteLine("Insira um número: ");
         int n = Convert.ToInt16(Console.ReadLine());
+
+        /*Testando o resultado
         for (int i = 0; i <= 35; i++){
             if (VerificarFibo(i)) Console.WriteLine(i + " pertence a sequência de Fibonacci");
             else Console.WriteLine(i + " não pertence a sequência de Fibonacci");
-        }
+        }*/
+
         if (VerificarFibo(n)) Console.WriteLine(n + " pertence a sequência de Fibonacci");
         else Console.WriteLine(n + " não pertence a sequência de Fibonacci");
     }
 
+    static private void Atividade03()
+    {
+        Console.WriteLine("Insira um texto: ");
+        string a = Console.ReadLine();
+
+        Console.Write("Invertida: ");
+        char[] letras = a.ToCharArray();
+
+        int tamanho = letras.Length;
+
+        for (int i = 0; i < tamanho / 2; i++)
+        {
+            char tmp = letras[i];
+            letras[i] = letras[tamanho - i - 1];
+            letras[tamanho - i - 1] = tmp;
+        }
+
+        Console.WriteLine(new string(letras));
+    }
     static bool VerificarFibo(int n)
     {
         int x = 0;
